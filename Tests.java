80d868fc299f0814dc45dede260c2afe1580bd16
KARTHIKEYAN816 <karthikshan706@gmail.com>
2022-02-06 09:47:13 +0530

    Junit Test

---
+++ Tests.java
@@ -0,0 +1,26 @@
+package git;
+
+import org.junit.Test;
+
+public class Tests {
+	@Test
+	public void test1() {
+		System.out.println("Test 1 is started");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test 2 is started");
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test 3 is started");
+	}
+	@Test
+	public void test4() {
+		System.out.println("Test 4 is started");
+	}
+	@Test
+	public void test5() {
+		System.out.println("Test 5 is started");
+	}
+}
